f6ec88d1cdcd221ad695880839cb30e8ac2ac741
Ray'J'D <Dike4mii4real@gmail.com>
2020-12-19 08:31:20 +0100

    update of extension

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import Countries from './src/components/screen/countries'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Countries);
